200a258eb368b2f868e25cef69d84df0c5354385
crisanch <cristian.sanchez@triciclos.cl>
2021-07-23 11:17:43 -0400

    chore(confi): add admin basic

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 2021_07_23_145458) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
