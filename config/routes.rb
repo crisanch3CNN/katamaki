6b29066c974bf00cf795b7517942587fe9f1ea3e
crisanch <cristian.sanchez@triciclos.cl>
2021-07-23 11:42:49 -0400

    chore(devide): config and add use

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root to: "home#index"
   scope path: '/api' do
     api_version(module: 'Api::V1', path: { value: 'v1' }, defaults: { format: 'json' }) do
     end
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   mount Sidekiq::Web => '/queue'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
